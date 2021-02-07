5b80a41033fc68501a8b0d2754438295649d4598
Alex Ghizila <alex@ghizila.de>
2021-02-07 22:38:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Greek Language Pack for [Flarum](https://flarum.org/)
+
+Extension to localize the Flarum forum software and its official extensions in Greek.
+
+## Information
+
+
+### Compatible with:
+
+- [Flarum](https://github.com/flarum/core) (Core v0.1.0-beta.15)
+  - [Akismet](https://github.com/flarum/flarum-ext-akismet)
+  - [Approval](https://github.com/flarum/flarum-ext-approval)
+  - [Emoji](https://github.com/flarum/emoji)
+  - [Flags](https://github.com/flarum/flags)
+  - [Likes](https://github.com/flarum/likes)
+  - [Lock](https://github.com/flarum/lock)
+  - [Markdown](https://github.com/flarum/markdown)
+  - [Mentions](https://github.com/flarum/mentions)
+  - [Nicknames](https://github.com/flarum/nicknames)
+  - [Pusher](https://github.com/flarum/pusher)
+  - [Statistics](https://github.com/flarum/statistics)
+  - [Sticky](https://github.com/flarum/sticky)
+  - [Subscriptions](https://github.com/flarum/subscriptions)
+  - [Suspend](https://github.com/flarum/suspend)
+  - [Tags](https://github.com/flarum/tags)
+
+
+
+
+## Documentation
+
+### How to install
+
+Use Composer in the root directory of your Flarum installation:
+
+```text
+composer require futuresound/flarum-el
+```
+
+See also on [Packagist](https://packagist.org/packages/futuresound/flarum-el).
+
+### How to update
+
+Run following commands in the root directory of your Flarum installation:
+
+```text
+composer update futuresound/flarum-el
+php flarum cache:clear
+```
+
+
+### License
+
+Released under the MIT License. Please see the [LICENSE](LICENSE) file.
---
+++ locale/config.js
@@ -0,0 +1,57 @@
+// Greek Day.js locale
+
+const texts = {
+    s: 'μερικά δευτερόλεπτα',
+    m: 'ένα λεπτό',
+    mm: '%d λεπτά',
+    h: 'μία ώρα',
+    hh: '%d ώρες',
+    d: 'μία μέρα',
+    dd: '%d μέρες',
+    M: 'ένα μήνα',
+    MM: '%d μήνες',
+    y: 'ένα χρόνο',
+    yy: '%d χρόνια'
+}
+
+function relativeTimeFormatter(number, withoutSuffix, key) {
+  let l = texts[key]
+  if (Array.isArray(l)) {
+    l = l[withoutSuffix ? 0 : 1]
+  }
+  return l.replace('%d', number)
+}
+
+dayjs.locale({
+    name: 'de',
+    weekdays: 'Κυριακή_Δευτέρα_Τρίτη_Τετάρτη_Πέμπτη_Παρασκευή_Σάββατο'.split('_'),
+    weekdaysShort: 'Κυρ_Δευ_Τρι_Τετ_Πεμ_Παρ_Σαβ'.split('_'),
+    weekdaysMin: 'Κυ_Δε_Τρ_Τε_Πε_Πα_Σα'.split('_'),
+    months: 'Ιανουάριος_Φεβρουάριος_Μάρτιος_Απρίλιος_Μάιος_Ιούνιος_Ιούλιος_Αύγουστος_Σεπτέμβριος_Οκτώβριος_Νοέμβριος_Δεκέμβριος'.split('_'),
+    monthsShort: 'Ιαν_Φεβ_Μαρ_Απρ_Μαι_Ιουν_Ιουλ_Αυγ_Σεπτ_Οκτ_Νοε_Δεκ'.split('_'),
+    ordinal: n => `${n}.`,
+    weekStart: 1,
+    formats: {
+      LTS: 'HH:mm:ss',
+      LT: 'HH:mm',
+      L: 'DD.MM.YYYY',
+      LL: 'D. MMMM YYYY',
+      LLL: 'D. MMMM YYYY HH:mm',
+      LLLL: 'dddd, D. MMMM YYYY HH:mm'
+    },
+    relativeTime: {
+      future: 'σε %s',
+      past: 'πριν %s',
+      s: relativeTimeFormatter,
+      m: relativeTimeFormatter,
+      mm: relativeTimeFormatter,
+      h: relativeTimeFormatter,
+      hh: relativeTimeFormatter,
+      d: relativeTimeFormatter,
+      dd: relativeTimeFormatter,
+      M: relativeTimeFormatter,
+      MM: relativeTimeFormatter,
+      y: relativeTimeFormatter,
+      yy: relativeTimeFormatter
+    }
+})
